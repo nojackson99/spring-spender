971960287f801a78dab40e92c432c4ea81e3f059
Noah Jackson <nojackson99@gmail.com>
2024-05-30 17:11:56 -0400

    add service and controller to facilitate initial testing, this is befor adding h2 placeholder dependency

--- api/spring.spender/src/main/java/app/java/spring/spender/initialtesting/InitialTestingController.java
+++ api/spring.spender/src/main/java/app/java/spring/spender/initialtesting/InitialTestingController.java
@@ -3,9 +3,29 @@ package app.java.spring.spender.initialtesting;
 // Create endpoints to test using the api to create data and get/post
 // to rds database
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("testing")
 public class InitialTestingController {
+
+    private final InitialTestingService initialTestingService;
+
+    public InitialTestingController(InitialTestingService initialTestingService) {
+        this.initialTestingService = initialTestingService;
+    }
+
     // POST seed initial data
+    @PostMapping("/seedPurchases")
+    public void seedPurchases() {
+        initialTestingService.seedPurchases();
+    }
 
     // GET get a purchase by id
+    @GetMapping("/getPurchase/{id}")
+    public void getPurchase(@PathVariable int id) {
+        initialTestingService.getPurchase(id);
+    }
 }
 
--- api/spring.spender/src/main/java/app/java/spring/spender/initialtesting/InitialTestingService.java
+++ api/spring.spender/src/main/java/app/java/spring/spender/initialtesting/InitialTestingService.java
@@ -1,4 +1,15 @@
 package app.java.spring.spender.initialtesting;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class InitialTestingService {
+    public void seedPurchases() {
+        System.out.println("Executing InitialTestingService.seedPurchases");
+    }
+
+    public void getPurchase(int purchaseId) {
+        System.out.println("Executing InitialTestingService.getPurchase");
+        System.out.println("Purchase Id is: " + purchaseId);
+    }
 }
